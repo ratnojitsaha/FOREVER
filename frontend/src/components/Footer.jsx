aa83ec04660452ea2d21225ff03e35e64a1b518f
Ratnojit Saha <saharatnojit04@gmail.com>
2025-10-17 13:52:34 +0530

    Initial Commit
    FIrst Commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img className="mb-5 w-32" src={assets.logo} alt="logo" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Forever was born out of a passion for innovation and a desire to revolutionize
+            the way people shop online. Our journey began with a simple idea — to provide a
+            platform where customers can easily discover, explore, and purchase a wide range
+            of products from the comfort of their homes.
+          </p>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+91 8145894267</li>
+            <li>saharatnojit04@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2025@ forever.com - All Right Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
